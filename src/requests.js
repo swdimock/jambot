284ca170f1b74e0e48451a34754c1eb501e075c6
Steve Dimock <steve@webgrain.net>
2021-12-30 22:25:57 -0700

    update: testing token

--- src/requests.js
+++ src/requests.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("¯\\_(ツ)_/¯");
+  res.status(200).send("¯\\_(ツ)_/¯");
 });
 
 app.get("/auth", async (req, res) => {
